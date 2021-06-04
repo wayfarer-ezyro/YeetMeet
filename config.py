5160290839d896ddefc492d4fcf50e8c8e0d290a
wayfarer-ezyro <72940489+wayfarer-ezyro@users.noreply.github.com>
2021-06-04 19:30:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 class Config(object):
-    BOT_TOKEN = os.environ.get('BOT_TOKEN')
-    GUSERNAME = os.environ.get('GUSER_NAME')
-    GPASSWORD = os.environ.get('GPASSWORD')
+    BOT_TOKEN = os.environ.get('1831209683:AAFNoDfTrJVIYeMLiKAdJG-rs7i7mbynh5I')
+    GUSERNAME = os.environ.get('ebymathew2005@gmail.com')
+    GPASSWORD = os.environ.get('ebymathew@05')
     SCHEDULE = os.environ.get('SCHEDULE', False)
-    USERID = os.environ.get('USERID')
+    USERID = os.environ.get('1453690249')
 # If you're not familiar with how to set Environment Variables you can do like this instead
 # of  setting Environment Variables
 
